dee0da6515a03bac608865710e1343721b4ba52a
Miwa <miwa.tnk9675@gmail.com>
2022-07-12 20:47:43 +0900

    created 404 page

--- audiophile/src/pages/404.js
+++ audiophile/src/pages/404.js
@@ -3,11 +3,19 @@ import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
+import * as styles from "../scss/_404.module.scss"
+
 const NotFoundPage = () => (
   <Layout>
     <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <section className={styles.pageWrapper}>
+      <h1>404</h1>
+      <h2>PAGE NOT FOUND</h2>
+      <p>Oops! The page you are looking for does not exsit. It might have been moved or deleted.</p>
+      <div className={styles.homeLink}>
+        <a href="/">BACK TO HOME</a>
+      </div>
+    </section>
   </Layout>
 )
 
