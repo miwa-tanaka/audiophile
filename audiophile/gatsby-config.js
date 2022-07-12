9e6e5ab6d44a39780b4f204b5bd80e70d0631684
Miwa <miwa.tnk9675@gmail.com>
2022-07-12 21:04:19 +0900

    changed favicon

--- audiophile/gatsby-config.js
+++ audiophile/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   pathPrefix: "/audiophile",
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `audiophile`,
+    description: `audiophile`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
@@ -30,7 +30,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
